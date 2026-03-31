c210b5e8a64a327a16f17b93c883063b572a30ea
Alex Day <aday@hey.com>
2026-03-31 07:35:10 +0100

    fixed scroll bug with blogs

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function RootLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="max-w-3xl mx-auto px-6 py-12">
       <Header />
